e20172d25fae216c1dbfd8327043ba65a57c43b6
Tiago Martins <grupomartinscomunicacao@gmail.com>
2025-08-20 16:18:51 -0300

    host alterado

--- setup/settings.py
+++ setup/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-(=-&$c%lq2!cxtmdwinj4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=Csv())
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='prismaav.pythonanywhere.com,localhost,127.0.0.1', cast=Csv())
 
 
 # Application definition
